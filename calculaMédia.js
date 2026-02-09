25c8ee4b2d23ebcc49d3039b4c6b02150c7a7ecb
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-02-09 17:20:34 +0000

    em teste

--- calculaMédia.js
+++ calculaMédia.js
@@ -10,8 +10,7 @@ let media = (nota1 + nota2 + nota3 + nota4) / 4
 if (media >= 7){
     console.log("O mailon está aprovado")
 } 
-if (media >= 5 && media <
-     7){
+if (media >= 5 && media < 7){
     console.log("O mailon está de se liga")
 }
 if (media < 5){
