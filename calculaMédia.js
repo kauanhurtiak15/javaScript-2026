9b658bed044f1a62ab2e754843e95f7869c9c0f4
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-02-09 17:12:46 +0000

    em teste

--- calculaMédia.js
+++ calculaMédia.js
@@ -7,5 +7,14 @@ const nomeAluno = "Mailon";
 
 let media = (nota1 + nota2 + nota3 + nota4) / 4
 
+if (media >= 7){
+    console.log("O aluno está aprovado")
+} if (media >= 5 && media >= 7){
+    console.log("O aluno está de se liga")
+}
+if (media < 5){
+    console.log("O aluno está reprovado")
+}
+
 console.log("O aluno: " + nomeAluno);
 console.log("Tem a média: " + media);
