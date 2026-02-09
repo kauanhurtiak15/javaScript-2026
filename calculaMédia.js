9d2bce29ad0603cad9e35e016aa1c9d62c76b4af
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-02-09 13:00:48 +0000

    em teste

---
+++ calculaMédia.js
@@ -0,0 +1,11 @@
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 4;
+let nota4 = 9;
+
+const nomeAluno = "Mailon";
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4
+
+console.log("O aluno: " + nomeAluno);
+console.log("Tem a média: " + media);
