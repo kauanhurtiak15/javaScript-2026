66667f097c68353deb0876ebe5a5fc96344bff34
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-03-04 12:56:04 +0000

    concluido

--- sistemaPub.js
+++ sistemaPub.js
@@ -6,14 +6,14 @@ const nome = readline.question("Qual seu nome? ");
 const idade = readline.questionInt("Qual sua idade? ");
 
 if (idade <= 18) {
-    console.log(`Olá ${nome}, sua entrada foi Autorizada`)
+    console.log(`Olá ${nome}, sua entrada foi Autorizada. `)
     const querBeber = readline.keyInYN("Deseja ver as bebidas? ");
     if (querBeber){
         console.log("Garçom á caminho! ");
     } else{
         console.log("Temos sucos também");
     }
-} else if (idade <=16){
+} else if (idade >=16){
     console.log(`Olá ${nome}, você prescisa estar acompanhado de um responsável! `);
 } else{
     console.log(`Sinto muito ${nome}, apenas maiores que 16 anos! `)
