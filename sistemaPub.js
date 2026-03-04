743b339a6734c94c20e4f9b2cafa47b2e2ab094b
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-03-04 13:04:12 +0000

    concluido

--- sistemaPub.js
+++ sistemaPub.js
@@ -1,20 +1,20 @@
 const readline = require('readline-sync');
 
-console.log("Sistema de acesso ao Pub do John");
+console.log("Sistema de acesso ao Pub do jhon");
 
 const nome = readline.question("Qual seu nome? ");
-const idade = readline.questionInt("Qual sua idade? ");
+const idade = readline.question("Qual sua idade? ");
 
-if (idade <= 18) {
+if (idade >= 18) {
     console.log(`Olá ${nome}, sua entrada foi Autorizada. `)
     const querBeber = readline.keyInYN("Deseja ver as bebidas? ");
     if (querBeber){
-        console.log("Garçom á caminho! ");
-    } else{
+        console.log("Garçom a caminho! ");
+    }else{
         console.log("Temos sucos também");
-}
-} else if (idade >=16){
-    console.log(`Olá ${nome}, você prescisa estar acompanhado de um responsável! `);
+    }
+}else if (idade >= 16){
+    console.log(`Olá ${nome}, você precisa estar acompanhado.. `);
 } else{
-    console.log(`Sinto muito ${nome}, apenas maiores que 16 anos! `)
+    console.log(`Sinto muito ${nome}, apenas maiores de 16 anos... `)
 }
