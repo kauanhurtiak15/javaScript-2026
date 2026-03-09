6fc9cdbe519a2121a0be14ff22708a102e7d4cff
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-03-09 17:29:26 +0000

    em teste

---
+++ precoIngresso.js
@@ -0,0 +1,17 @@
+const readline = require('readline-sync');
+
+let nome = "kauan";
+let idade = 16;
+let temCupon= true;
+let precoFinal= " ";
+
+const PRECO_CHEIO = 30;
+
+if (idade >= 60, precoFinal = PRECO_CHEIO / 2);
+
+else if (idade < 18, temCupon = true, precoFinal = PRECO_CHEIO * 0.8);
+
+else (precoFinal === PRECO_CHEIO);
+
+console.log(`Olá, ${nome}`);
+console.log(`o valor do seu ingresso é ${precoFinal}`);
