725706f61dcec90491cee2c43df98ce57237e359
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-03-11 12:39:48 +0000

    EM ANDAMENTO

--- semaforo.js
+++ semaforo.js
@@ -1,1 +1,6 @@
-let corSemaforo = "verde"
+let corSemaforo = verde;
+let veiculoEmergencia = true;
+
+if (veiculoEmergencia == true){
+    console.log("ALERTA: anbulância detectada! todos os sinais fechados. CRUZAMENTO LIVRE.");
+}
