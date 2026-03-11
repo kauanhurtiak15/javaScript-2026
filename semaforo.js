efa65b45138a29ca86aa63ee41d894362135f2a3
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-03-11 12:47:00 +0000

    semaforo

--- semaforo.js
+++ semaforo.js
@@ -1,6 +1,14 @@
-let corSemaforo = verde;
+let corSemaforo = "verde";
+let cor = "amarelo";
 let veiculoEmergencia = true;
 
 if (veiculoEmergencia == true){
     console.log("ALERTA: anbulância detectada! todos os sinais fechados. CRUZAMENTO LIVRE.");
+} else if(corSemaforo == verde){
+    console.log("Pode passar! Siga com cuidado.");
+} else if(cor == amarelo){
+    console.log("Atenção! Reduza a velocidade e prepare-se para parar.")
+}
+else{
+    console.log("PARE! Aguarde o sinal verde.")
 }
