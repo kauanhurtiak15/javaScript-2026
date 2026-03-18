633b2880ec01564a65480ebad0384905cd5130c3
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-03-18 13:06:32 +0000

    ok

--- vetor.js
+++ vetor.js
@@ -1,14 +1,16 @@
 let compras = ["Morango", "Banana", "Pão", "Leite", "Queijo"];
 
-console.log(compras)
+//console.log(compras)
 
-console.log("O primeiro item da lista é " + compras[0]);
-console.log("O último item da lista é " + compras[4]);
+//console.log("O primeiro item da lista é " + compras[0]);
+//console.log("O último item da lista é " + compras[4]);
 
 compras[2] = "Macarrão";
-console.log(compras[2]);
+//console.log(compras[2]);
 compras.push("Pepsi");
 compras.push("Limão");
-console.log(compras);
+//console.log(compras);
 compras.pop();
-console.log("Compras");
+//console.log("Compras");
+
+console.log(compras.length)
